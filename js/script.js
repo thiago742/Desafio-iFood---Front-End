19a677422bec122e2fb6ed10fc98172cb90321cf
Thiago F <fthiago742@gmail.com>
2024-09-09 23:19:44 -0300

    perto de acabar

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
 document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+document.getElementById("toggleButton").addEventListener("click", function() {
+    var content = document.getElementById("hiddenContent");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+});
